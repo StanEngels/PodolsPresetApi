668361df0695608607853f7554cf3c742798134f
StanEngels <stanengels@gmail.com>
2020-11-05 17:08:24 +0100

    dotnet Build fix

--- PresetApi/PresetApi.test/UnitTest1.cs
+++ PresetApi/PresetApi.test/UnitTest1.cs
@@ -6,7 +6,6 @@ using System.Collections.Generic;
 using System.Linq;
 using Microsoft.EntityFrameworkCore;
 using PresetApi.Controllers;
-using MockQueryable.FakeItEasy;
 using System.Threading.Tasks;
 using PresetApi.Interfaces;
 using Microsoft.AspNetCore.Mvc;
