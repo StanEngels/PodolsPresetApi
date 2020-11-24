c44cb3517614660ebe6cc10c929e37da083e3468
StanEngels <stanengels@gmail.com>
2020-11-24 11:06:09 +0100

    sonarqube test

--- PresetApi/PresetApi.test/UnitTest1.cs
+++ PresetApi/PresetApi.test/UnitTest1.cs
@@ -26,7 +26,7 @@ namespace PresetApi.test
             ActionResult<List<Preset>> result = await presetController.GetPresets();
 
             //Assert
-            Assert.NotNull(result.Value);
+            Assert.Null(result.Value);
             Assert.Equal(result.Value[0].presetName, expectedPreset[0].presetName);
         }
 
